66b7e92b92b42c5396a1edb4e422b8265735a21b
Sally8192 <zl3229@nyu.edu>
2024-09-07 15:16:44 -0400

    HW-1

---
+++ ps-1/plot_gaussian.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+
+mean = 0
+std = 3
+x_range = np.linspace(-10, 10, 100)
+
+def gaussian(x, mu, sigma):
+    value = 1.0 / (np.sqrt(2.0 * np.pi) * sigma) * np.exp(-np.power((x - mu) / sigma, 2.0) / 2)
+    return value 
+
+plt.xlabel("Values over the range [-10, 10]")
+plt.ylabel("Normalized Gaussian value")
+plt.title("Gussian Curve")
+plt.plot(x_range, gaussian(x_range, mean, std))
+plt.savefig('gaussian.png')
+plt.show()
